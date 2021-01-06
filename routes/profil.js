a6bcc591726c4a26b2184a93f446b6e440cd0eea
Mojtabarezaei4 <54796834+Mojtabarezaei4@users.noreply.github.com>
2021-01-06 13:16:35 +0100

    Made a "Personal profil", works as admin

--- routes/profil.js
+++ routes/profil.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+var name = "Johan"
+
 router.get('/', (req, res) => {
-    res.render('profil/Profil')
+    res.render('profil/PersonalProfil', {name : name})
 })
 
 module.exports = router
